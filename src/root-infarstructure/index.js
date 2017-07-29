d69aea4bde3933b696dc3201285b9d26c0ed06e6
Apollo Tang <tang.apollo@gmail.com>
2017-07-29 15:20:42 -0400

    linked up redux

--- src/root-infarstructure/index.js
+++ src/root-infarstructure/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-// import { ConnectedRouter } from 'connected-react-router';
-// import {history} from './createHistory';
-// import store from './store';
-// import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import {history} from './create-history';
+import store from './store';
+import { Provider } from 'react-redux';
 
 import {BrowserRouter} from 'react-router-dom';
 
@@ -11,23 +11,23 @@ import RootApp from 'root-app';
 
 import style from './style';
 
-// const Root = ()=>(
-//   <div className={`root ${style['module-style']}`}>
-//     <Provider store={store}>
-//       <ConnectedRouter history={history}>
-//         <RootApp />
-//       </ConnectedRouter>
-//     </Provider>
-//   </div>
-// );
-
 const Root = ()=>(
   <div className={`root ${style['module-style']}`}>
-    <BrowserRouter>
-      <RootApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <RootApp />
+      </ConnectedRouter>
+    </Provider>
   </div>
 );
+
+// const Root = ()=>(
+//   <div className={`root ${style['module-style']}`}>
+//     <BrowserRouter>
+//       <RootApp />
+//     </BrowserRouter>
+//   </div>
+// );
 
 export default Root;
 
