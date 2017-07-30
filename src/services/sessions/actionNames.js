fc0812262aa5c68475754a571f6e7ac6eca44862
Apollo Tang <tang.apollo@gmail.com>
2017-07-30 00:32:27 -0400

    clean up; wip transfer container

--- src/services/sessions/actionNames.js
+++
@@ -1,8 +0,0 @@
-const resources = [
-  'sessions_authenticate_createSession_success',
-  'sessions_authenticate_createSession_fail',
-  'sessions_authenticate_destroySession'
-];
-
-export default resources;
-
