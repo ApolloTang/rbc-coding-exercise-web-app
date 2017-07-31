b1d298503cc8f8b78d73856850cf590ebd4ed239
Apollo Tang <tang.apollo@gmail.com>
2017-07-31 05:43:13 -0400

    add email and password form

--- src/app-ui/containers/login/index.js
+++ src/app-ui/containers/login/index.js
@@ -47,19 +47,52 @@ class Screen_Login extends React.Component {
   }
 
   render() {
-
-    if (this.props.isLoading) {
-      return(
-        <div>
-          <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
-        </div>
-      )
-    };
-
     return (
       <div>
-        loggin in screen
+        <div className="group">
+          <div className="form-row" data-fn="email" >
+            <div className="label-wrap left-col">
+              <label id="email_label" htmlFor="email">
+                email:
+              </label>
+            </div>
+            <div className="input-wrap last-col">
+              <input
+                onChange={ ()=>{} }
+                type="text"
+                id="email"
+                aria-labelledby="email_label"
+                defaultValue={''}
+              />
+            </div>
+          </div>
+
+          <div className="form-row" data-fn="password" >
+            <div className="label-wrap left-col">
+              <label id="password_label" htmlFor="password">
+                Password:
+              </label>
+            </div>
+            <div className="input-wrap last-col">
+              <input
+                onChange={ ()=>{} }
+                type="password"
+                id="password"
+                aria-labelledby="password_label"
+                defaultValue={''}
+              />
+            </div>
+          </div>
+        </div>
         <button onClick={this.handle_login} > log in </button>
+        {
+          (this.props.isLoading) ?
+            <div className='message'>
+              <div className='wrap-spinner'>
+                <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
+              </div>
+            </div> : null
+        }
       </div>
     );
   }
